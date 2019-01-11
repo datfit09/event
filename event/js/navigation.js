9e1d4f99aa8c2a31ad193337073ca25e0bd494cc
PhungCongDat <phungdat.nd@gmail.com>
2019-01-11 11:23:26 +0700

    8th commit

--- event/js/navigation.js
+++ event/js/navigation.js
@@ -8,16 +8,40 @@
         menu        = $('nav ul'),
         menuHeight  = menu.height();
  
-    pull.on('click', function(e) {
+    pull.on('click', function( e ) {
         e.preventDefault();
         menu.slideToggle();
     });
 
     $(window).resize(function(){
         var w = $(window).width();
-        if(w > 768 && menu.is(':hidden')) {
+        if(w > 991 && menu.is(':hidden')) {
             menu.removeAttr('style');
         }
     });
 })( jQuery );
 
+jQuery( document.body ).on( 'click', '.primary-menu .menu-item a', function( e ) {
+
+    e.preventDefault();
+
+    var t         = jQuery( this ),
+        next      = t.next(),
+        nextAlias = t.parent().parent().find( 'li .sub-menu' );
+
+    // Go to url if not have sub-menu.
+    if ( ! t.siblings().length ) {
+        // hash.
+        window.location.href = t.prop( 'href' );
+    }
+
+    if ( next.hasClass( 'show' ) ) {
+        next.removeClass( 'show' );
+        next.slideUp( 200 );
+    } else {
+        nextAlias.removeClass( 'show' );
+        nextAlias.slideUp( 200 );
+        next.toggleClass( 'show' );
+        next.slideToggle( 200 );
+    }
+});
